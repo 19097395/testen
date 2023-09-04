2d7b12cd25ee9c42d10d27f91f54b9a4f6fe8cd0
19097395 <56761664+19097395@users.noreply.github.com>
2023-09-04 16:16:29 +0200

    Update t.js

--- t.js
+++ t.js
@@ -1,6 +1,29 @@
-function h()
+async function h()
 {
 	alert('hi');
-	const res = await fetch('');
+	try 
+	{
+    		const response = await fetch("https://media.tenor.com/Nv4ctEkehj8AAAAC/whos-the-best-hacker-the-flash.gif");
+    		if (!response.ok) 
+		{
+     			throw new Error("Network response was not OK");
+    		}
+		const myImage = new Image(100, 200);
+
+    		const myBlob = await response.blob();
+   		myImage.src = URL.createObjectURL(myBlob);
+		document.body.appendChild(myImage);
+
+  	}
+	catch (error) 
+	{
+    		console.error("Error:", error);
+  	}
+	
+	
+	
+}
+function resp(z)
+{
 	
 }
